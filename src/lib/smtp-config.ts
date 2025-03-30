9b7f7e8a91f740f8f21ffaa6b2fdcf8ed23e3319
Thibault Vercoutre <vercoutre.thibault@gmail.com>
2025-03-30 15:30:16 -0400

    Mise à jour de la configuration SMTP : changement du port à 587 et désactivation de la sécurité pour utiliser STARTTLS. Modification du mot de passe par défaut pour les tests.

--- src/lib/smtp-config.ts
+++ src/lib/smtp-config.ts
@@ -1,7 +1,7 @@
 export const smtpConfig = {
   host: 'ssl0.ovh.net', // ou smtp.ovh.net
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false, // false pour le port 587 avec STARTTLS
   user: process.env.SMTP_USER || 'dash@web-gine.fr',
-  password: process.env.SMTP_PASSWORD || 'votre-mot-de-passe-ici', // À remplacer par le vrai mot de passe pour les tests
+  password: process.env.SMTP_PASSWORD || 'UfZ99sf0r0cbP2',
 }; 
