2bd0f51246dac9eaed770c7ddf76f97261715764
Thibault Vercoutre <vercoutre.thibault@gmail.com>
2025-04-03 11:13:18 -0400

    Suppression de l'importation de SensorAlertInfo dans le fichier route.ts, simplifiant ainsi le code et se concentrant sur l'envoi d'emails groupés d'alertes.

--- src/app/api/ttn-webhook/route.ts
+++ src/app/api/ttn-webhook/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 import { PrismaClient } from '@prisma/client';
-import { SensorAlertInfo, sendMultipleAlertsEmail } from '@/lib/email';
+import { sendMultipleAlertsEmail } from '@/lib/email';
 
 const prisma = new PrismaClient();
 
