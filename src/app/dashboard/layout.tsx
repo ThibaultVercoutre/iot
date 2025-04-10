30fa9ef78efd7cd21c5212f4b3dc0f45843e19fe
Thibault Vercoutre <vercoutre.thibault@gmail.com>
2025-04-10 12:12:59 -0400

    Suppression de plusieurs fichiers SVG inutilisés dans le répertoire public et ajout d'une image dans le layout du tableau de bord pour remplacer le texte du titre.

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button"
 import Link from "next/link"
+import Image from "next/image"
 
 export default function DashboardLayout({
   children,
@@ -13,7 +14,10 @@ export default function DashboardLayout({
           <div className="flex justify-between h-16">
             <div className="flex">
               <div className="flex-shrink-0 flex items-center">
-                <span className="text-xl font-bold">IoT Dashboard</span>
+                <span className="text-xl font-bold">
+                  <Image src="/logoDashName.png" alt="Logo" width={32} height={32} />
+                    IoT Dashboard
+                  </span>
               </div>
             </div>
             <div className="flex items-center">
