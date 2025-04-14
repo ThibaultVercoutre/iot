c750c14f8aba6a05024d88dea7a35a31b224d825
Thibault Vercoutre <vercoutre.thibault@gmail.com>
2025-04-14 15:52:42 -0400

    Modification de la visibilité des étiquettes dans le composant NavbarDocumentation pour qu'elles apparaissent uniquement sur les écrans larges, améliorant ainsi l'expérience utilisateur sur les appareils de différentes tailles.

--- src/components/dashboard/documentation/NavbarDocumentation.tsx
+++ src/components/dashboard/documentation/NavbarDocumentation.tsx
@@ -3,26 +3,26 @@ import { Home, AlertCircle, Settings, PieChart, Tablet } from "lucide-react"
 
 export default function NavbarDocumentation() {
   return (
-    <TabsList className="grid grid-cols-3 lg:grid-cols-5 gap-2">
+    <TabsList className="grid grid-cols-5 gap-2">
       <TabsTrigger value="dashboard" className="flex items-center gap-2 cursor-pointer">
         <Home className="h-4 w-4" />
-        <span className="hidden md:inline">Tableau de bord</span>
+        <span className="hidden lg:inline">Tableau de bord</span>
       </TabsTrigger>
       <TabsTrigger value="devices" className="flex items-center gap-2 cursor-pointer">
         <Tablet className="h-4 w-4" />
-        <span className="hidden md:inline">Appareils et capteurs</span>
+        <span className="hidden lg:inline">Appareils et capteurs</span>
       </TabsTrigger>
       <TabsTrigger value="alerts" className="flex items-center gap-2 cursor-pointer">
         <AlertCircle className="h-4 w-4" />
-        <span className="hidden md:inline">Alertes</span>
+        <span className="hidden lg:inline">Alertes</span>
       </TabsTrigger>
       <TabsTrigger value="account" className="flex items-center gap-2 cursor-pointer">
         <Settings className="h-4 w-4" />
-        <span className="hidden md:inline">Compte</span>
+        <span className="hidden lg:inline">Compte</span>
       </TabsTrigger>
       <TabsTrigger value="stats" className="flex items-center gap-2 cursor-pointer">
         <PieChart className="h-4 w-4" />
-        <span className="hidden md:inline">Statistiques</span>
+        <span className="hidden lg:inline">Statistiques</span>
       </TabsTrigger>
     </TabsList>
   )
